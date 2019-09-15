ca509af368561cb660937794f8495d6fef7c2915
Rudolf Fedorov <52194526+rurraka@users.noreply.github.com>
2019-09-15 17:31:51 +0300

    Revert "Revert "Merge pull request #11 from rurraka/master""
    This reverts commit bfc51ac138d34a5d1ed2e62a127c7beda0c932d3.

---
+++ app/models/spree/taxon_interior.rb
@@ -0,0 +1,21 @@
+module Spree
+  class TaxonInterior < Asset
+    #include Configuration::ActiveStorage
+    include Rails.application.routes.url_helpers
+
+    def styles
+      self.class.styles.map do |_, size|
+        width, height = size[/(\d+)x(\d+)/].split('x')
+
+        {
+          url: polymorphic_path(attachment.variant(resize: size), only_path: true),
+          width: width,
+          height: height
+        }
+      end
+    end
+	def display_name
+		alt.blank? ? attachment.filename : alt
+	end
+  end
+end
