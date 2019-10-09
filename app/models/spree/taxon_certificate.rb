658b69374e4bc851cf2a819df04b9b473b4ae0eb
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-09 15:50:09 +0300

    Allow all types of certs

--- app/models/spree/taxon_certificate.rb
+++ app/models/spree/taxon_certificate.rb
@@ -1,22 +1,35 @@
 module Spree
   class TaxonCertificate < Asset
-    include Configuration::ActiveStorage
-	include Rails.application.routes.url_helpers
+    #include Configuration::ActiveStorage
+	
+  validate :check_attachment_presence
+
+  has_one_attached :attachment
+
+  def check_attachment_presence
+    unless attachment.attached?
+      attachment.purge
+      errors.add(:attachment, :attachment_must_be_present)
+    end
+  end
+
 
 	#self.inheritance_column = nil
-    def styles
-      self.class.styles.map do |_, size|
-        width, height = size[/(\d+)x(\d+)/].split('x')
+   # def styles
+   #   self.class.styles.map do |_, size|
+   #     width, height = size[/(\d+)x(\d+)/].split('x')
 
-        {
-          url: polymorphic_path(attachment.variant(resize: size), only_path: true),
-          width: width,
-          height: height
-        }
-      end
-    end
-	def display_name
-		alt.blank? ? attachment.filename : alt
-	end
+#        {
+#          url: polymorphic_path(attachment.variant(resize: size), only_path: true),
+#          width: width,
+#          height: height
+#        }
+#      end
+#    end
+  	include Rails.application.routes.url_helpers
+
+    def display_name
+  		alt.blank? ? attachment.filename : alt
+  	end
   end
 end
