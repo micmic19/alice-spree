5c1408cb0b88e85cdead213a24493fa6b9fd1a54
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-14 14:31:26 +0300

    remove duplicate code

--- app/models/spree/taxon_certificate.rb
+++ app/models/spree/taxon_certificate.rb
@@ -2,18 +2,6 @@ module Spree
   class TaxonCertificate < Asset
     include Configuration::ActiveStorage
 	
-  validate :check_attachment_presence
-
-  has_one_attached :attachment
-
-  def check_attachment_presence
-    unless attachment.attached?
-      attachment.purge
-      errors.add(:attachment, :attachment_must_be_present)
-    end
-  end
-
-
 	# self.inheritance_column = nil
     def styles
       self.class.styles.map do |_, size|
