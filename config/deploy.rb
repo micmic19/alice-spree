975df0297090a411fd43d11cf5bcdb0d6f081d82
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-10-30 17:50:40 +0300

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,4 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+set :nginx_use_ssl, true
