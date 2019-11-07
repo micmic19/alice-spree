dffb7daa0cdbb69d53b37a198f53dfa579aeef35
micmic19 <45197167+micmic19@users.noreply.github.com>
2019-11-07 10:58:28 +0300

    replace reqiest.domain to request.host

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def company_phone
-    domainname = request.domain
-    if domainname.include? 'mskopt.alice.ru'
+    domainname = request.host
+    if domainname.include? 'mskopt'
       return '+7 (495) 287-81-41'
     else
       return '+7 (812) 331-14-60'
@@ -9,8 +9,8 @@ module ApplicationHelper
   end
 
   def company_phone1
-    domainname = request.domain
-    if domainname.include? 'mskopt.alice.ru'
+    domainname = request.host
+    if domainname.include? 'mskopt'
       return '+7 (495) 287-81-39'
     else
       return '+7 (921) 740-66-92'
@@ -18,8 +18,8 @@ module ApplicationHelper
   end
 
   def company_email
-    domainname = request.domain
-    if domainname.include? 'mskopt.alice.ru'
+    domainname = request.host
+    if domainname.include? 'mskopt'
       return 'sale@msk.alice.ru'
     else
       return 'sale@alice.ru'
@@ -27,8 +27,8 @@ module ApplicationHelper
   end
 
   def company_title
-    domainname = request.domain
-    if domainname.include? 'mskopt.alice.ru'
+    domainname = request.host
+    if domainname.include? 'mskopt'
       return 'Линолеум оптом'
     else
       return 'Линолеум оптом'
@@ -36,8 +36,8 @@ module ApplicationHelper
   end
 
   def step_option_id
-    domainname = request.domain
-    if domainname.include? 'mskopt.alice.ru'
+    domainname = request.host
+    if domainname.include? 'mskopt'
       return 4
     else
       return 4
